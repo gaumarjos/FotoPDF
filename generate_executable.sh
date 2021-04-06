b5d271af35aa55bc5cb2d277d3312c267825a1cb
Stefano Salati <ste.salati@protonmail.com>
2021-04-06 19:49:37 +0200

    PyInstaller config for GUI version

--- generate_executable.sh
+++ generate_executable.sh
@@ -1,9 +1,11 @@
 clear
-rm -r dist
+rm -rf build dist
 pyinstaller --noconfirm --log-level=ERROR \
     --onefile \
-    --nowindow \
+    --windowed \
     --name FotoPDF \
     --paths ~/PycharmProjects/FotoPDF/venv/lib/python3.7/site-packages \
     --add-data font_default.ttf:./font_default.ttf \
+    --add-data FotoPDF.png:./FotoPDF.png \
+    --icon FotoPDF.png \
     main.py
