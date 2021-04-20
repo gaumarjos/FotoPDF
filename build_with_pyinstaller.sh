92e245520d3064551a5ded020dbda81f39298de2
Stefano Salati <ste.salati@protonmail.com>
2021-04-20 16:35:51 +0200

    Trying without success to make pyinstaller work

--- build_with_pyinstaller.sh
+++ build_with_pyinstaller.sh
@@ -5,10 +5,16 @@ pyinstaller --noconfirm --log-level=ERROR \
     --onefile \
     --windowed \
     --name FotoPDF \
-    --debug all \
+    --debug imports \
     --paths ~/PycharmProjects/FotoPDF/venv2/lib/python3.9/site-packages/ \
     --paths ~/PycharmProjects/FotoPDF/venv2/ \
+    --paths ~/PycharmProjects/FotoPDF/venv2/lib/python3.9/site-packages/PyQt5/ \
     --add-data font_default.ttf:. \
     --add-data FotoPDF.png:. \
     --icon FotoPDF.png \
+    --hidden-import PyQt5 \
+    --hidden-import PyQt5.sip \
+    --hidden-import PyQt5.QtWidgets \
+    --hidden-import PyQt5.QtGui \
+    --hidden-import PyQt5.QtCore \
     FotoPDF.py
